ac1950c756e97465a933c41fce6c5a06728c3c9b
Cao Pao Lo <caoanh.2406.1995@gmail.com>
2017-02-03 10:59:42 +0700

    caoa

---
+++ Bkademy/Webpos/Block/AbstractBlock.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: anhnc
+ * Date: 24/01/2017
+ * Time: 13:20
+ */
+namespace Bkademy\Webpos\Block;
+class AbstractBlock extends \Magento\Framework\View\Element\Template
+{
+
+}
